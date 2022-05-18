c8d60b2c751d677e2176d9f352b5597ba9f2cab5
Carlos Reyes <carlos.reyes@itksquare.edu.mx>
2022-05-18 08:08:08 -0500

    Repo to get token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseConfig } from './firebaseconf';
+import { initializeApp } from "firebase/app";
 import './App.css';
 
+const firebaseApp = initializeApp(firebaseConfig);
+
+const aut = getAuth(firebaseApp);
+
+
 function App() {
+  const email = "admin@test.com"
+  const passd = "test123"
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(aut, email, passd)
+      console.log(user);
+    } catch (error) {
+      console.error(error)
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button  onClick={login}>Log in</button>
     </div>
   );
 }
---
+++ src/firebaseconf.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcEzc8Oc70ZjCy8FidMSj_CS-GyE_uox4",
+  authDomain: "itk-5g-users.firebaseapp.com",
+  projectId: "itk-5g-users",
+  storageBucket: "itk-5g-users.appspot.com",
+  messagingSenderId: "762159116897",
+  appId: "1:762159116897:web:0315c70f65a87de664f020"
+};
+
+// Initialize Firebase
